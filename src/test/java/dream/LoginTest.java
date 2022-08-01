a6a6bad5ebdbd330f63fb0e16b721fdb140571d3
srimukeshrockz <105345110+srimukeshrockz@users.noreply.github.com>
2022-08-01 17:23:55 +0530

    Update LoginTest.java

--- src/test/java/dream/LoginTest.java
+++ src/test/java/dream/LoginTest.java
@@ -10,7 +10,7 @@ public class LoginTest extends Base {
     @Test
     public void clickOnView()
     {
-        //this is the change
+        //second change
         LoginPage lpage=new LoginPage(driver);
         lpage.clickOnContinue();
         lpage.clickOnOkayButton();
